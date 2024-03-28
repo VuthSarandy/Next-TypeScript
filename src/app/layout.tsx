ad754ac6604e6ba7f171713764c83f352f0a8e28
VuthSarandy <dysart9999@gmail.com>
2024-03-28 14:54:47 +0700

    sabay jit vin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,9 @@ export const metadata: Metadata = {
   openGraph: {
     title: "SarandyShop",
     description: "This is description shop",
-    images: [ 
+    images: 
       "https://cdn.pixabay.com/photo/2023/09/02/18/07/escalator-8229360_1280.jpg",
-    ],
+
   },
 };
 
