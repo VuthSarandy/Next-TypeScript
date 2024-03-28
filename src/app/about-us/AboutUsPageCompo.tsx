257da0c3ef738173f9d706146430466f7cc34a0a
VuthSarandy <dysart9999@gmail.com>
2024-03-28 09:32:19 +0700

    fix some code

--- src/app/about-us/AboutUsPageCompo.tsx
+++ src/app/about-us/AboutUsPageCompo.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+
+
 const AboutUsCompo = () => {
   return (
     <div>
--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import AboutUsCompo from './aboutUsPageCompo'
+import AboutUsCompo from './AboutUsPageCompo'
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
